50554fca45852540ed7c63a2c533d4e94ae6dfa1
Maxx Nikitin <maxxnikitin90@yandex.ru>
2021-03-12 10:41:10 +0300

    Разворачивание проекта, добавление папки burger-ui

---
+++ src/burger-ui/ui/icons/index.tsx
@@ -0,0 +1,47 @@
+import { CurrencyIcon } from './currency-icon';
+import { BurgerIcon } from './burger-icon';
+import { LockIcon } from './lock-icon';
+import { DragIcon } from './drag-icon';
+import { CloseIcon } from './close-icon';
+import { CheckMarkIcon } from './check-mark-icon';
+import { ListIcon } from './list-icon';
+import { ProfileIcon } from './profile-icon';
+import { EditIcon } from './edit-icon';
+import { InfoIcon } from './info-icon';
+import { ShowIcon } from './show-icon';
+import { HideIcon } from './hide-icon';
+import { LogoutIcon } from './logout-icon';
+import { TIconProps } from './utils';
+import React from 'react';
+
+export type TICons = {
+    CurrencyIcon: React.FC<TIconProps>;
+    BurgerIcon: React.FC<TIconProps>;
+    LockIcon: React.FC<TIconProps>;
+    DragIcon: React.FC<TIconProps>;
+    CloseIcon: React.FC<TIconProps>;
+    CheckMarkIcon: React.FC<TIconProps>;
+    ListIcon: React.FC<TIconProps>;
+    ProfileIcon: React.FC<TIconProps>;
+    EditIcon: React.FC<TIconProps>;
+    InfoIcon: React.FC<TIconProps>;
+    ShowIcon: React.FC<TIconProps>;
+    HideIcon: React.FC<TIconProps>;
+    LogoutIcon: React.FC<TIconProps>;
+};
+
+export {
+    CurrencyIcon,
+    BurgerIcon,
+    LockIcon,
+    DragIcon,
+    CloseIcon,
+    CheckMarkIcon,
+    ListIcon,
+    ProfileIcon,
+    EditIcon,
+    InfoIcon,
+    ShowIcon,
+    HideIcon,
+    LogoutIcon,
+};
