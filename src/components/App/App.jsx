5ed30961e041248281ed65db8ba39b7718de60e3
Maxx Nikitin <m.nikitin@goodcustomers.ru>
2021-03-24 11:28:02 +0700

    начало вёрстки ингредиентов

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,14 @@
 import AppHeader from "../AppHeader/AppHeader";
 import "./App.css";
+import BurgerIngredients from '../BurgerIngredients/BurgerIngredients';
 
 function App() {
   return (
     <div className="app">
       <AppHeader />
+      <main className="main">
+        <BurgerIngredients />
+      </main>
     </div>
   );
 }
--- src/components/BurgerIngredients/BurgerIngredients.jsx
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -1,2 +1,25 @@
 import React from "react";
 import "./BurgerIngredients.css";
+import { Tab } from '../../burger-ui/ui';
+
+class BurgerIngredients extends React.Component {
+  render() {
+    return (
+      <section className="burger-ingredients">
+        <h1 className="burger-ingredients__title">Соберите бургер</h1>
+        <nav>
+          <ul className="burger-ingredients__menu">
+            <li><Tab text='Булки' type='current' /></li>
+            <li><Tab text='Соусы' /></li>
+            <li><Tab text='Начинки' /></li>
+          </ul> 
+        </nav>
+        <div className="burger-ingredients__content">
+          
+        </div>
+      </section>
+    )
+  }
+}
+
+export default BurgerIngredients;
