3a45cb40a60a11f14fcfabfbc0a76e25762857c6
Maxx Nikitin <m.nikitin@goodcustomers.ru>
2021-03-25 11:11:51 +0700

    фикс вёрстки ингредиентов

--- src/components/BurgerIngredients/BurgerIngredients.jsx
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./BurgerIngredients.css";
-import { Tab } from '../../burger-ui/ui';
+import { ConstructorElement, Tab } from '../../burger-ui/ui';
+import { CurrencyIcon, DragIcon, LockIcon } from "../../burger-ui/ui/icons";
+import { Counter } from "../../burger-ui/ui/counter";
 
 class BurgerIngredients extends React.Component {
   render() {
     return (
       <section className="burger-ingredients">
-        <h1 className="burger-ingredients__title">Соберите бургер</h1>
+        <h1 className="text text_type_main-large mt-5 mb-3">Соберите бургер</h1>
         <nav>
           <ul className="burger-ingredients__menu">
             <li><Tab text='Булки' type='current' /></li>
@@ -15,57 +17,64 @@ class BurgerIngredients extends React.Component {
           </ul> 
         </nav>
         <div className="burger-ingredients__content">
-          <h3 className='burger-ingredients__categories'>Булки</h3>
+          <h3 className='text text_type_main-medium mt-5 mb-3'>Булки</h3>
           <div className='burger-ingredients__items'>
             <article className='burger-ingredients__article'>
+              <Counter count='1' />
               <img className='burger-ingredients__img' src='#' alt='картинка ингредиента.' />
-              <div className='burger-ingredients__cost'>
-                <p className='burger-ingredients__count'>20</p>
+              <div className='burger-ingredients__cost mt-1 mb-1'>
+                <p className='text text_type_digits-default mr-1'>20</p>
+                <CurrencyIcon />
               </div>
-              <p className='burger-ingredients__description'>Краторная булка N-200i</p>
+              <p className='text text_type_main-default'>Краторная булка N-200i</p>
             </article>
             <article className='burger-ingredients__article'>
               <img className='burger-ingredients__img' src='#' alt='картинка ингредиента.' />
-              <div className='burger-ingredients__cost'>
-                <p className='burger-ingredients__count'>20</p>
+              <div className='burger-ingredients__cost mt-1 mb-1'>
+                <p className='text text_type_digits-default mr-1'>20</p>
+                <CurrencyIcon />
               </div>
-              <p className='burger-ingredients__description'>Флюоресцентная булка R2-D3</p>
+              <p className='text text_type_main-default'>Флюоресцентная булка R2-D3</p>
             </article>
           </div>
-          <h3 className='burger-ingredients__categories'>Соусы</h3>
+          <h3 className='text text_type_main-medium mt-5 mb-3'>Соусы</h3>
           <div className='burger-ingredients__items'>
           <article className='burger-ingredients__article'>
               <img className='burger-ingredients__img' src='#' alt='картинка ингредиента.' />
-              <div className='burger-ingredients__cost'>
-                <p className='burger-ingredients__count'>30</p>
+              <div className='burger-ingredients__cost mt-1 mb-1'>
+                <p className='text text_type_digits-default mr-1'>30</p>
+                <CurrencyIcon />
               </div>
-              <p className='burger-ingredients__description'>Соус Spicy-X</p>
+              <p className='text text_type_main-default'>Соус Spicy-X</p>
             </article>
             <article className='burger-ingredients__article'>
               <img className='burger-ingredients__img' src='#' alt='картинка ингредиента.' />
-              <div className='burger-ingredients__cost'>
-                <p className='burger-ingredients__count'>30</p>
+              <div className='burger-ingredients__cost mt-1 mb-1'>
+                <p className='text text_type_digits-default mr-1'>30</p>
+                <CurrencyIcon />
               </div>
-              <p className='burger-ingredients__description'>Соус фирменный Space Sauce</p>
+              <p className='text text_type_main-default'>Соус фирменный Space Sauce</p>
             </article>
             <article className='burger-ingredients__article'>
               <img className='burger-ingredients__img' src='#' alt='картинка ингредиента.' />
-              <div className='burger-ingredients__cost'>
-                <p className='burger-ingredients__count'>30</p>
+              <div className='burger-ingredients__cost mt-1 mb-1'>
+                <p className='text text_type_digits-default mr-1'>30</p>
+                <CurrencyIcon />
               </div>
-              <p className='burger-ingredients__description'>Краторная булка N-200i</p>
+              <p className='text text_type_main-default'>Краторная булка N-200i</p>
             </article>
             <article className='burger-ingredients__article'>
               <img className='burger-ingredients__img' src='#' alt='картинка ингредиента.' />
-              <div className='burger-ingredients__cost'>
-                <p className='burger-ingredients__count'>30</p>
+              <div className='burger-ingredients__cost mt-1 mb-1'>
+                <p className='text text_type_digits-default mr-1'>30</p>
+                <CurrencyIcon />
+                
               </div>
-              <p className='burger-ingredients__description'>Флюоресцентная булка R2-D3</p>
+              <p className='text text_type_main-default'>Флюоресцентная булка R2-D3</p>
             </article>
-          </div>
-          <h3 className='burger-ingredients__categories'>Начинки</h3>
-          <div className='burger-ingredients__items'>
-
+            <LockIcon />
+            <ConstructorElement />
+            <DragIcon />
           </div>
         </div>
       </section>
